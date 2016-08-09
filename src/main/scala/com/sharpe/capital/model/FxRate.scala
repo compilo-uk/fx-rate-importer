5188d1f76105764cb719ce9ad19afc4f6a5b93ab
Lewis Barber <lewis_barber@hotmail.com>
2016-08-09 22:36:22 +0100

    Undo Cassandra stuff

--- src/main/scala/com/sharpe/capital/model/FxRate.scala
+++ src/main/scala/com/sharpe/capital/model/FxRate.scala
@@ -1,9 +1,26 @@
 package com.sharpe.capital.model
 
-import org.joda.time.DateTime
+import java.util.Date
 
-case class FxRate(
-  symbol: String,
-  ask: BigDecimal,
-  bid: BigDecimal,
-  date: DateTime)
+/**
+ * Model class to wrap FX quote information
+ */
+class FxRate(askPrice: BigDecimal, bidPrice: BigDecimal, tickerSymbol: String, quoteDate: Date) {
+
+  def bid(): BigDecimal = {
+    return bidPrice
+  }
+
+  def ask(): BigDecimal = {
+    return askPrice
+  }
+
+  def symbol(): String = {
+    return tickerSymbol
+  }
+
+  def date(): Date = {
+    return quoteDate
+  }
+
+}
